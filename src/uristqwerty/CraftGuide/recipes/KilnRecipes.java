19e162adefde6101cb0c788bed3968ebec03f1b9
Joel Paul <joel.v.paul@outlook.com>
2021-12-12 12:26:54 +1100

    Improve KilnRecipes function, fixed weird recipe inputs

--- src/uristqwerty/CraftGuide/recipes/KilnRecipes.java
+++ src/uristqwerty/CraftGuide/recipes/KilnRecipes.java
@@ -3,11 +3,16 @@ package uristqwerty.CraftGuide.recipes;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 import net.minecraft.src.Block;
 import net.minecraft.src.FCBetterThanWolves;
+import net.minecraft.src.FCBlockUnfiredPottery;
+import net.minecraft.src.FCCraftingManagerBulkRecipe;
 import net.minecraft.src.FCCraftingManagerKiln;
 import net.minecraft.src.FCCraftingManagerKilnRecipe;
+import net.minecraft.src.FCItemPlacesAsBlock;
+import net.minecraft.src.Item;
 import net.minecraft.src.ItemStack;
 import uristqwerty.CraftGuide.api.CraftGuideAPIObject;
 import uristqwerty.CraftGuide.api.ItemSlot;
@@ -18,8 +23,6 @@ import uristqwerty.CraftGuide.api.Slot;
 import uristqwerty.CraftGuide.api.SlotType;
 
 public class KilnRecipes extends CraftGuideAPIObject implements RecipeProvider {
-	private Slot[] slots;
-	
 	private ItemStack bellows = new ItemStack(FCBetterThanWolves.fcBellows);
 	private ItemStack brick = new ItemStack(Block.brick);
 	private ItemStack hibachi = new ItemStack(FCBetterThanWolves.fcBBQ);
@@ -27,36 +30,52 @@ public class KilnRecipes extends CraftGuideAPIObject implements RecipeProvider {
 
 	@Override
 	public void generateRecipes(RecipeGenerator generator) {
-		createSlots();
-		RecipeTemplate template = generator.createRecipeTemplate(slots, brick);
-		
 		try {
 			Field recipesField = FCCraftingManagerKiln.class.getDeclaredField("recipes");
 			recipesField.setAccessible(true);
 			ArrayList<FCCraftingManagerKilnRecipe> recipes = (ArrayList<FCCraftingManagerKilnRecipe>) recipesField.get(FCCraftingManagerKiln.instance);
 			
-			HashSet<ItemStack> completedRecipes = new HashSet();
-			
+			int outputSize = 0;
 			for (FCCraftingManagerKilnRecipe recipe : recipes) {
-				// TODO: Find out why some Unfired Pottery blocks don't render properly.
-				// Might be connected to why there are multiple bread/cookie recipes?
+				ItemStack[] outputs = recipe.getOutput();
+				outputSize = Math.max(outputSize, outputs.length);
+			}
+			Slot[] slots = BTWRecipes.createSlots(1, 3, outputSize);
+			RecipeTemplate template = generator.createRecipeTemplate(slots, brick);
+			
+			HashSet<Integer> completedInputs = new HashSet();
+			for (FCCraftingManagerKilnRecipe recipe : recipes) {
+
+				Block inputBlock = recipe.getInputblock();
+				int metadata = recipe.getInputMetadata();
+				// We get the item dropped, otherwise it'll give us the Block IDs of the
+				// actual blocks placed in the world, which aren't legitimately obtainable.
+				int inputID = inputBlock.idDropped(metadata, null, 1);
 				
-				ItemStack input = new ItemStack(recipe.getInputblock(), 1, recipe.getInputMetadata());
+				// The consequence of the above method is that wet brick and
+				// unfired Nether brick drop clay and Nether sludge respectively.
+				if (inputID == Item.clay.itemID) {
+					inputID = FCBetterThanWolves.fcItemBrickUnfired.itemID;
+				}
+				if (inputID == FCBetterThanWolves.fcItemNetherSludge.itemID) {
+					inputID = FCBetterThanWolves.fcItemNetherBrickUnfired.itemID;
+				}
 				
-				// Skip duplicates.
-				if (!completedRecipes.add(input)) {
+				// Skip duplicate inputs.
+				if (!completedInputs.add(inputID)) {
 					continue;
 				}
 				
+				ItemStack input = new ItemStack(inputID, 1, 0);
 				ItemStack[] crafting = new ItemStack[slots.length];
+				
 				crafting[0] = input;
 				for (int i = 0; i < 3; i++) {
 					crafting[1 + i] = machines[i];
 				}
 				ItemStack[] output = recipe.getOutput();
-				int j = output.length == 1 ? 1 : 0;
 				for (int i = 0; i < 3 && i < output.length; i++) {
-					crafting[4 + i + j] = output[i];
+					crafting[4 + i] = output[i];
 				}
 				
 				generator.addRecipe(template, crafting);
@@ -69,20 +88,6 @@ public class KilnRecipes extends CraftGuideAPIObject implements RecipeProvider {
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
 			e.printStackTrace();
-		}
-	}
-	
-	private void createSlots() {
-		slots = new ItemSlot[7];
-		int xOffset = 14;
-		
-		slots[0] = new ItemSlot(xOffset, 21, 16, 16, true);
-		
-		for (int i = 0; i < 3; i++) {
-			slots[1 + i] = new ItemSlot(18 + xOffset, i * 18 + 3, 16, 16).setSlotType(SlotType.MACHINE_SLOT);
-		}
-		for (int i = 0; i < 3; i++) {
-			slots[4 + i] = new ItemSlot(36 + xOffset, i * 18 + 3, 16, 16, true).setSlotType(SlotType.OUTPUT_SLOT);
 		}
 	}
 }
