762b20c57f1b6e1ced5da7a88fca9d21afc8feed
Joel Paul <joel.v.paul@outlook.com>
2021-12-12 16:57:40 +1100

    Streamline TurntableRecipes code

--- src/uristqwerty/CraftGuide/recipes/TurntableRecipes.java
+++ src/uristqwerty/CraftGuide/recipes/TurntableRecipes.java
@@ -21,12 +21,11 @@ import uristqwerty.CraftGuide.api.Slot;
 import uristqwerty.CraftGuide.api.SlotType;
 
 public class TurntableRecipes extends CraftGuideAPIObject implements RecipeProvider {
-	private Slot[] slots;
 	private ItemStack turntable = new ItemStack(FCBetterThanWolves.fcTurntable);
 
 	@Override
 	public void generateRecipes(RecipeGenerator generator) {
-		createSlots();
+		Slot[] slots = BTWRecipes.createSlots(1, 1, 2);
 		RecipeTemplate template = generator.createRecipeTemplate(slots, turntable);
 		
 		generator.addRecipe(template, new ItemStack[] {
@@ -34,39 +33,24 @@ public class TurntableRecipes extends CraftGuideAPIObject implements RecipeProvi
 			turntable,
 			new ItemStack(FCBetterThanWolves.fcUnfiredPottery, 1, FCBlockUnfiredPottery.m_iSubtypeCrucible),
 			new ItemStack(Item.clay),
-			null
 		});
 		generator.addRecipe(template, new ItemStack[] {
 			new ItemStack(FCBetterThanWolves.fcUnfiredPottery, 1, FCBlockUnfiredPottery.m_iSubtypeCrucible),
 			turntable,
 			new ItemStack(FCBetterThanWolves.fcUnfiredPottery, 1, FCBlockUnfiredPottery.m_iSubtypePlanter),
 			new ItemStack(Item.clay, 2),
-			null
 		});
 		generator.addRecipe(template, new ItemStack[] {
 			new ItemStack(FCBetterThanWolves.fcUnfiredPottery, 1, FCBlockUnfiredPottery.m_iSubtypePlanter),
 			turntable,
 			new ItemStack(FCBetterThanWolves.fcUnfiredPottery, 1, FCBlockUnfiredPottery.m_iSubtypeVase),
 			new ItemStack(Item.clay, 2),
-			null
 		});
 		generator.addRecipe(template, new ItemStack[] {
 			new ItemStack(FCBetterThanWolves.fcUnfiredPottery, 1, FCBlockUnfiredPottery.m_iSubtypeVase),
 			turntable,
 			new ItemStack(FCBetterThanWolves.fcUnfiredPottery, 1, FCBlockUnfiredPottery.m_iSubtypeUrn),
 			new ItemStack(Item.clay, 2),
-			null
 		});
-	}
-	
-	private void createSlots() {
-		slots = new ItemSlot[5];
-		int xOffset = 14;
-		
-		slots[0] = new ItemSlot(xOffset, 21, 16, 16);
-		slots[1] = new ItemSlot(18 + xOffset, 21, 16, 16).setSlotType(SlotType.MACHINE_SLOT);
-		for (int i = 0; i < 3; i++) {
-			slots[2 + i] = new ItemSlot(36 + xOffset, i * 18 + 3, 16, 16, true).setSlotType(SlotType.OUTPUT_SLOT);
-		}
 	}
 }
