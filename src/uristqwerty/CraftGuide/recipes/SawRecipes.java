9223771fe0572604af7958220e351da1702093e2
Joel Paul <joel.v.paul@outlook.com>
2021-12-12 15:03:45 +1100

    Fix issues with SawRecipes and improve code

--- src/uristqwerty/CraftGuide/recipes/SawRecipes.java
+++ src/uristqwerty/CraftGuide/recipes/SawRecipes.java
@@ -3,10 +3,14 @@ package uristqwerty.CraftGuide.recipes;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
+import net.minecraft.src.Block;
 import net.minecraft.src.FCBetterThanWolves;
+import net.minecraft.src.FCCraftingManagerPistonPackingRecipe;
 import net.minecraft.src.FCCraftingManagerSaw;
 import net.minecraft.src.FCCraftingManagerSawRecipe;
+import net.minecraft.src.Item;
 import net.minecraft.src.ItemStack;
 import uristqwerty.CraftGuide.api.CraftGuideAPIObject;
 import uristqwerty.CraftGuide.api.ItemSlot;
@@ -17,29 +21,58 @@ import uristqwerty.CraftGuide.api.Slot;
 import uristqwerty.CraftGuide.api.SlotType;
 
 public class SawRecipes extends CraftGuideAPIObject implements RecipeProvider {
-	private Slot[] slots;
 	private ItemStack saw = new ItemStack(FCBetterThanWolves.fcSaw);
 
 	@Override
-	public void generateRecipes(RecipeGenerator generator) {
-		createSlots();
-		RecipeTemplate template = generator.createRecipeTemplate(slots, saw);
-		
+	public void generateRecipes(RecipeGenerator generator) {;
 		try {
 			Field recipesField = FCCraftingManagerSaw.class.getDeclaredField("recipes");
 			recipesField.setAccessible(true);
 			ArrayList<FCCraftingManagerSawRecipe> recipes = (ArrayList<FCCraftingManagerSawRecipe>) recipesField.get(FCCraftingManagerSaw.instance);
 			
-			// TODO: Figure out why some 'weird' recipes appear, i.e. wood type changing when sawed.
+			int outputSize = 0;
 			for (FCCraftingManagerSawRecipe recipe : recipes) {
+				ItemStack[] outputs = recipe.getOutput();
+				outputSize = Math.max(outputSize, outputs.length);
+			}
+			Slot[] slots = BTWRecipes.createSlots(1, 1, outputSize);
+			
+			int outputW = (int) Math.ceil(outputSize / 3.0);
+			int outputH = Math.min(outputSize, 3);
+			int maxHeight = Math.max(outputH, 1);
+			
+			RecipeTemplate template = generator.createRecipeTemplate(slots, saw).setSize((2 + outputW) * 18 + 6, maxHeight * 18 + 6);
+			
+			HashSet<List<Integer>> completedInputs = new HashSet();
+			for (FCCraftingManagerSawRecipe recipe : recipes) {
+				Block inputBlock = recipe.getInputblock();
+				int metadata = recipe.getInputMetadata();
+				// We get the item dropped, otherwise it'll give us the Block IDs of the
+				// actual blocks placed in the world, which aren't legitimately obtainable.
+				// (i.e. various orientations of the "same" block).
+				int inputID = inputBlock.idDropped(metadata, null, 1);
+				metadata = inputBlock.damageDropped(metadata);
+				
+				// For some reason idDropped for vines is 0.
+				if (inputID == 0) {
+					inputID = Block.vine.blockID;
+				}
+				
+				// Skip duplicate inputs.
+				ArrayList<Integer> inputCheck = new ArrayList();
+				inputCheck.add(inputID);
+				inputCheck.add(metadata);
+				if (!completedInputs.add(inputCheck)) {
+					continue;
+				}
+				
 				ItemStack[] crafting = new ItemStack[slots.length];
 				
-				crafting[0] = new ItemStack(recipe.getInputblock(), 1, recipe.getInputMetadata());
+				crafting[0] = new ItemStack(inputID, 1, metadata);
 				crafting[1] = saw;
 				ItemStack[] outputs = recipe.getOutput();
-				int j = outputs.length == 1 ? 1 : 0;
-				for (int i = 0; i < 3 && i < outputs.length; i++) {
-					crafting[2 + i + j] = outputs[i];
+				for (int i = 0; i < outputs.length; i++) {
+					crafting[2 + i] = outputs[i];
 				}
 				
 				generator.addRecipe(template, crafting);
@@ -52,17 +85,6 @@ public class SawRecipes extends CraftGuideAPIObject implements RecipeProvider {
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
 			e.printStackTrace();
-		}
-	}
-	
-	private void createSlots() {
-		slots = new ItemSlot[5];
-		int xOffset = 14;
-		
-		slots[0] = new ItemSlot(xOffset, 21, 16, 16);
-		slots[1] = new ItemSlot(18 + xOffset, 21, 16, 16).setSlotType(SlotType.MACHINE_SLOT);
-		for (int i = 0; i < 3; i++) {
-			slots[2 + i] = new ItemSlot(36 + xOffset, i * 18 + 3, 16, 16, true).setSlotType(SlotType.OUTPUT_SLOT);
 		}
 	}
 }
