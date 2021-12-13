71eb0d3561dc0f01c37b79e830dfe8ba80adb429
Joel Paul <joel.v.paul@outlook.com>
2021-12-13 12:23:55 +1100

    Add all wool colors

--- src/uristqwerty/CraftGuide/recipes/KnittingRecipes.java
+++ src/uristqwerty/CraftGuide/recipes/KnittingRecipes.java
@@ -1,6 +1,7 @@
 package uristqwerty.CraftGuide.recipes;
 
 import net.minecraft.src.FCBetterThanWolves;
+import net.minecraft.src.FCItemWool;
 import net.minecraft.src.ItemStack;
 import uristqwerty.CraftGuide.api.CraftGuideAPIObject;
 import uristqwerty.CraftGuide.api.ItemSlot;
@@ -15,32 +16,34 @@ import uristqwerty.CraftGuide.api.SlotType;
 
 public class KnittingRecipes extends CraftGuideAPIObject implements RecipeProvider {
 	private ItemStack needles = new ItemStack(FCBetterThanWolves.fcItemKnittingNeedles);
-	private ItemStack wool = new ItemStack(FCBetterThanWolves.fcItemWool);
 	private ItemStack knitting = new ItemStack(FCBetterThanWolves.fcItemKnitting);
 
 	@Override
 	public void generateRecipes(RecipeGenerator generator) {
 		Slot[] slots = createSlots();
-		RecipeTemplate template = generator.createRecipeTemplate(slots, null).setSize(4 * 18 + 6, 2 * 18 + 6);
+		RecipeTemplate template = generator.createRecipeTemplate(slots, null).setSize(4 * 18 + 8, 3 * 18 + 4);
 		
-		ItemStack[] crafting = new ItemStack[slots.length];
-		
-		crafting[0] = wool;
-		crafting[1] = wool;
-		crafting[2] = needles;
-		crafting[4] = knitting;
-		
-		generator.addRecipe(template, crafting);
+		for (int i = 0; i < FCItemWool.m_iWoolColors.length; i++) {
+			ItemStack[] crafting = new ItemStack[slots.length];
+			ItemStack wool = new ItemStack(FCBetterThanWolves.fcItemWool, 1, i);
+			
+			crafting[0] = wool;
+			crafting[1] = wool;
+			crafting[2] = needles;
+			crafting[4] = knitting;
+			
+			generator.addRecipe(template, crafting);
+		}
 	}
 	
 	// Taken from DefaultRecipeProvider and modified.
 	private Slot[] createSlots() {
 		return new ItemSlot[]{
-			new ItemSlot(12, 12, 16, 16),
-			new ItemSlot(30, 12, 16, 16),
-			new ItemSlot(12, 30, 16, 16),
-			new ItemSlot(30, 30, 16, 16),
-			new ItemSlot(59, 21, 16, 16, true).setSlotType(SlotType.OUTPUT_SLOT),
+			new ItemSlot(12, 12, 16, 16).drawOwnBackground(),
+			new ItemSlot(30, 12, 16, 16).drawOwnBackground(),
+			new ItemSlot(12, 30, 16, 16).drawOwnBackground(),
+			new ItemSlot(30, 30, 16, 16).drawOwnBackground(),
+			new ItemSlot(59, 21, 16, 16, true).drawOwnBackground().setSlotType(SlotType.OUTPUT_SLOT),
 		};
 	}
 }
