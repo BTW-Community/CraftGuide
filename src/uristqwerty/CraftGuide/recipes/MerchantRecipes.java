9fe21a60bb21aa7fabca256b11979d474070b55b
Joel Paul <joel.v.paul@outlook.com>
2021-12-14 15:51:21 +1100

    Add level up trade recipes

--- src/uristqwerty/CraftGuide/recipes/MerchantRecipes.java
+++ src/uristqwerty/CraftGuide/recipes/MerchantRecipes.java
@@ -23,34 +23,43 @@ import uristqwerty.CraftGuide.api.Slot;
 public class MerchantRecipes extends CraftGuideAPIObject implements RecipeProvider {
 	private Set<Integer> professions = FCEntityVillager.professionMap.keySet();
 	
-	private Map<Integer, Set<WeightedMerchantEntry>> tradeByProfessionList = FCEntityVillager.tradeByProfessionList;
-	private Map<Integer, Map<Integer, WeightedMerchantEntry>> levelUpTradeByProfessionList = FCEntityVillager.levelUpTradeByProfessionList;
 	private Map<Integer, WeightedMerchantEntry> defaultTradeByProfessionList = FCEntityVillager.defaultTradeByProfessionList;
 	
 	@Override
 	public void generateRecipes(RecipeGenerator generator) {
-		Slot[] slots = BTWRecipes.createSlots(2, 1, 1, false);
-		RecipeTemplate template = generator.createRecipeTemplate(slots, null).setSize(3 * 18 + 6, 2 * 18 + 4);
-		
 		for (int profession : professions) {
-			Set<WeightedMerchantEntry> entries = tradeByProfessionList.get(profession);
+			Set<WeightedMerchantEntry> entries = FCEntityVillager.tradeByProfessionList.get(profession);
 			for (WeightedMerchantEntry entry : entries) {
-				try {
-					ItemStack[] crafting = new ItemStack[slots.length];
-					MerchantRecipe recipe = entry.generateRecipe(new Random());
-					
-					crafting[0] = recipe.getItemToBuy();
-					crafting[1] = recipe.getSecondItemToBuy();
-					crafting[2] = new ItemStack(Item.monsterPlacer, 1, 600 + profession);
-					crafting[3] = recipe.getItemToSell();
-					
-					generator.addRecipe(template, crafting);
-				} catch (IllegalArgumentException e) {
-					e.printStackTrace();
-				} catch (SecurityException e) {
-					e.printStackTrace();
-				}
+				addEntryRecipe(generator, entry, profession);
 			}
+			
+			for (int level = 1; level < 5; level++) {
+				WeightedMerchantEntry entry = FCEntityVillager.levelUpTradeByProfessionList.get(profession).get(level);
+				addEntryRecipe(generator, entry, profession);
+			}
+		}
+	}
+	
+	private void addEntryRecipe(RecipeGenerator generator, WeightedMerchantEntry entry, int profession) {
+		if (entry == null) return;
+		
+		Slot[] slots = BTWRecipes.createSlots(2, 1, 1, false);
+		RecipeTemplate template = generator.createRecipeTemplate(slots, new ItemStack(Item.emerald)).setSize(3 * 18 + 6, 2 * 18 + 4);
+		
+		try {
+			MerchantRecipe recipe = entry.generateRecipe(new Random());
+			ItemStack[] crafting = new ItemStack[slots.length];
+			
+			crafting[0] = recipe.getItemToBuy();
+			crafting[1] = recipe.getSecondItemToBuy();
+			crafting[2] = new ItemStack(Item.monsterPlacer, 1, 600 + profession);
+			crafting[3] = recipe.getItemToSell();
+			
+			generator.addRecipe(template, crafting);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
 		}
 	}
 }
